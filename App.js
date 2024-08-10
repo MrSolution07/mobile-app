80e8a89f6fffc894eeeec27b671926d7464007d1
M.solution <97136008+MrSolution07@users.noreply.github.com>
2024-08-10 19:21:02 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc'; // this is a how you import tailwindcss in react native
+
+export default function App() {
+  return (
+    <View style={[styles.container, tw`bg-blue-600`]}>
+      <Text style={[styles.headerText, tw`text-white`]}>
+        Welcome to React Native!
+      </Text>
+      <Text style={[styles.bodyText, tw`text-gray-200`]}>
+        This is a sample application using Tailwind.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  bodyText: {
+    fontSize: 16,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+});
